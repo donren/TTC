fdc31916b310ac658f8620711fa7097f2ebdd700
donren <rene.lueth@t-online.de>
2020-12-12 20:07:30 +0100

    add print data

--- server/server.py
+++ server/server.py
@@ -20,6 +20,7 @@ while True:
  print('Connected by', addr)
  try:
         data = conn.recv(1024)
+        print(data)
         if data == b"get_id":
             g_id = get_id()
             conn.sendto(g_id.encode(),address=addr)
