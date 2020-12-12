fb1e855df33afd7f8dc5d90391024f380ebb75e9
Rene <rene.lueth@t-online.de>
2020-12-12 18:45:11 +0000

    add server

---
+++ server/server.py
@@ -0,0 +1,19 @@
+import socket
+
+host = ''        # Symbolic name meaning all available interfaces
+port = 12345     # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+print host , port
+while True:
+ s.listen(1)
+ conn, addr = s.accept()
+ print('Connected by', addr)
+ try:
+        data = conn.recv(1024)
+        print "Client Says: "+data
+        conn.sendall("Server Says:hi")
+
+ except:
+       pass
