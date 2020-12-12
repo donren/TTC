d301e96cb52df6f7da92ce8c514c8fcc14d2c4c9
donren <rene.lueth@t-online.de>
2020-12-12 21:26:40 +0100

    sync

--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@ host = ''        # Symbolic name meaning all available interfaces
 port = 12345     # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
+global id
 
 def get_id():
     id += 1
